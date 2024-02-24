8dbe169a5a28b12ce91831397c9c41cc7d724952
Oscar-0713 <nctse@connect.ust.hk>
2024-02-25 00:05:32 +0800

    [Update] Update the interpolation dp number as dynamic

--- library.h
+++ library.h
@@ -423,7 +423,7 @@ public:
 // with the key encoded as type N
 // [2,1] pair indicates its error (or all [l,r] s.t. r < l) is error or invalid.
     std::pair<N, N> GetValue(N key) {
-        std::cout << "Getting value of " << key << std::endl;
+//        std::cout << "Getting value of " << key << std::endl;
         assert(key >= segments_[0].x_start);
         if (segments_.empty()) {
             return std::pair<N, N>();
